2d785ec64a7eb6822300b99ce5efb596fe6d3daa
Kristian Brodal <kristianbrodal@gmail.com>
2019-04-03 14:32:41 +0200

    First commit of Gonzales, the new ECS pathfinding solution.

---
+++ Gonzales/Assets/Gonzales/Scripts/Components/NavMeshSeeker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Gonzales.Components
+{
+    public class NavMeshSeeker : IComponentData
+    {
+    }
+}
+
